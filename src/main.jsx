2243b3b54327e25e8e5808641227820473f31c03
Shamimul143 <shamimul143@gmail.com>
2024-03-25 19:44:41 +0600

    all pages creat done

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './root/Root';
+import HomePage from './pages/HomePage';
+import ListedBooks from './pages/ListedBooks';
+import PagesToRead from './pages/PagesToRead';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>,
+      },
+      {
+        path: "/listedBook",
+        element: <ListedBooks></ListedBooks>,
+      },
+      {
+        path: "/pagesToRead",
+        element: <PagesToRead></PagesToRead>,
+      },
+    ]
   },
 ]);
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
---
+++ src/pages/PagesToRead.jsx
@@ -0,0 +1,11 @@
+
+
+const PagesToRead = () => {
+    return (
+        <div>
+            <h1>Page to read</h1>
+        </div>
+    );
+};
+
+export default PagesToRead;
